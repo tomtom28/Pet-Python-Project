37b65b21a964b00529932f08513c5c0bed380a4b
Tommy Thompson <tomthompson828@gmail.com>
2017-02-09 23:08:49 -0500

    fixed the port. it was hard coded to 5000

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=PORT)
