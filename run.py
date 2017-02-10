acd42c0974ecc2d0d452463b17deabcd37828a44
Tommy Thompson <tomthompson828@gmail.com>
2017-02-09 23:28:48 -0500

    deploy

--- run.py
+++ run.py
@@ -6,5 +6,6 @@ config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    PORT = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=PORT)
+    # PORT = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=PORT)
+    app.run()
