d78c48d95a5b4581d22512d3104536131c222c1f
Tommy Thompson <tomthompson828@gmail.com>
2017-06-30 23:18:23 -0400

    infosec is NOT overrated

--- instance/config.py
+++ instance/config.py
@@ -1,7 +1,7 @@
 # Determine if in Development or Production
 import os
 FLASK_CONFIG = os.environ.get('FLASK_CONFIG')
-
+JAWSDB_URL = os.environ.get('JAWSDB_URL')
 
 # Configuration Variable - Secret Key for root admin user. Not needed at the moment.
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
@@ -9,6 +9,6 @@ SECRET_KEY = 'p9Bv<3Eid9%$i01'
 
 # Use Localhost if connected locally in development or JAWS_DB if in production
 if (FLASK_CONFIG == 'production'):
-    SQLALCHEMY_DATABASE_URI = 'mysql://v32slj6qn71ei419:aksjdk01livfeb5p@alv4v3hlsipxnujn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zrdbrnk8afyxyoev'
+    SQLALCHEMY_DATABASE_URI = JAWSDB_URL
 else:
     SQLALCHEMY_DATABASE_URI = 'mysql://dt_admin:dt2016@localhost/dreamteam_db'
